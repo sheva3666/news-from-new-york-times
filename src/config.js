042a1a66e64daf7d7ea8d62e6d1d57131d43615d
sheva3666 <ivan.shevchenko@sap.com>
2024-03-10 22:48:41 +0100

    api key

--- src/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  API_KEY: "EBP00jVujgZO0059Eyg0w5dv45eximYE",
-};
--- src/utils/getApiUrl.js
+++ src/utils/getApiUrl.js
@@ -1,5 +1,3 @@
-import { API_KEY } from "../config";
-
 export const apiUrlTypes = {
   news: "NEWS",
   global: "GLOBAL",
@@ -7,8 +5,8 @@ export const apiUrlTypes = {
 
 export const getApiUrl = ({ type, category, searchItem }) => {
   if (type === apiUrlTypes.news) {
-    return `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${API_KEY}`;
+    return `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${process.env.API_KEY}`;
   } else {
-    return `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchItem}&api-key=${API_KEY}`;
+    return `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchItem}&api-key=${process.env.API_KEY}`;
   }
 };
